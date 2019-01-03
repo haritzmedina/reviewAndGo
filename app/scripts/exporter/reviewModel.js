ba8dd3ba8fe5d3f27daff343a466f780bf190e2c
Haritz Medina <haritzmedina@gmail.com>
2019-01-03 10:52:09 +0100

    Disable eslint in this files

--- app/scripts/exporter/reviewModel.js
+++ app/scripts/exporter/reviewModel.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 
 class Review {
   constructor(){
--- app/scripts/specific/review/ReviewGenerator.js
+++ app/scripts/specific/review/ReviewGenerator.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const axios = require('axios')
 const _ = require('lodash')
 const Alerts = require('../../utils/Alerts')
